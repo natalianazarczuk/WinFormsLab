cab58a91f24ee4452467ac0534c6fca4af6498d2
Natalia Nazarczuk <01151472@pw.edu.pl>
2022-04-16 21:51:36 +0200

    Add project files.

---
+++ PiGE.02/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace PiGE._02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGameItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGameItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.NewGameItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.CreateItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableleft = new System.Windows.Forms.TableLayoutPanel();
+            this.tabletop = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItem1,
+            this.menuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(978, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewGameItem1,
+            this.NewGameItem2,
+            this.NewGameItem3});
+            this.menuItem1.Name = "menuItem1";
+            this.menuItem1.Size = new System.Drawing.Size(114, 29);
+            this.menuItem1.Text = "New Game";
+            // 
+            // NewGameItem1
+            // 
+            this.NewGameItem1.Name = "NewGameItem1";
+            this.NewGameItem1.Size = new System.Drawing.Size(241, 34);
+            this.NewGameItem1.Text = "Random";
+            this.NewGameItem1.Click += new System.EventHandler(this.NewGameItem1_Click);
+            // 
+            // NewGameItem2
+            // 
+            this.NewGameItem2.Enabled = false;
+            this.NewGameItem2.Name = "NewGameItem2";
+            this.NewGameItem2.Size = new System.Drawing.Size(241, 34);
+            this.NewGameItem2.Text = "Choose puzzle...";
+            // 
+            // NewGameItem3
+            // 
+            this.NewGameItem3.Enabled = false;
+            this.NewGameItem3.Name = "NewGameItem3";
+            this.NewGameItem3.Size = new System.Drawing.Size(241, 34);
+            this.NewGameItem3.Text = "Load puzzle";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateItem1});
+            this.menuItem2.Name = "menuItem2";
+            this.menuItem2.Size = new System.Drawing.Size(90, 29);
+            this.menuItem2.Text = "Create...";
+            // 
+            // CreateItem1
+            // 
+            this.CreateItem1.Enabled = false;
+            this.CreateItem1.Name = "CreateItem1";
+            this.CreateItem1.Size = new System.Drawing.Size(219, 34);
+            this.CreateItem1.Text = "Create puzzle";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(350, 250);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 300);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableleft
+            // 
+            this.tableleft.ColumnCount = 5;
+            this.tableleft.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableleft.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableleft.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableleft.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableleft.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableleft.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tableleft.Location = new System.Drawing.Point(234, 250);
+            this.tableleft.Name = "tableleft";
+            this.tableleft.RowCount = 10;
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableleft.Size = new System.Drawing.Size(115, 300);
+            this.tableleft.TabIndex = 2;
+            // 
+            // tabletop
+            // 
+            this.tabletop.AccessibleRole = System.Windows.Forms.AccessibleRole.StaticText;
+            this.tabletop.ColumnCount = 10;
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tabletop.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tabletop.Location = new System.Drawing.Point(350, 134);
+            this.tabletop.Name = "tabletop";
+            this.tabletop.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tabletop.RowCount = 5;
+            this.tabletop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tabletop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tabletop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tabletop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tabletop.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tabletop.Size = new System.Drawing.Size(300, 115);
+            this.tabletop.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 744);
+            this.Controls.Add(this.tabletop);
+            this.Controls.Add(this.tableleft);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nonogram";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menuItem2;
+        private System.Windows.Forms.ToolStripMenuItem NewGameItem1;
+        private System.Windows.Forms.ToolStripMenuItem NewGameItem2;
+        private System.Windows.Forms.ToolStripMenuItem NewGameItem3;
+        private System.Windows.Forms.ToolStripMenuItem CreateItem1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableleft;
+        private System.Windows.Forms.TableLayoutPanel tabletop;
+    }
+}
+
---
+++ PiGE.02/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiGE._02
+{
+    public partial class Form1 : Form
+    {
+        private Button[] buttons;
+        private Form newForm;
+        TextBox width;
+        TextBox height;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int width = 10;
+            int height = 10;
+            buttons = new Button[100];
+
+            for (int i = 0; i < width * height; i++)
+            {
+                Random rnd = new Random();
+                int x = rnd.Next(0, 2);
+
+                buttons[i] = new Button()
+                {
+                    Dock = DockStyle.Fill,
+                    FlatStyle = FlatStyle.Flat,
+                    Margin = Padding.Empty,
+                    BackColor = (x == 0) ? Color.Black : Color.White
+                };
+                buttons[i].Click += button_Click;
+
+                tableLayoutPanel1.Controls.Add(buttons[i], i / width, i % height);
+
+                button_Click(buttons[i], e);
+            }
+
+            //left labels
+            for (int i = 0; i < height; i++)
+            {
+                int equal = 0;
+                int col = 4;
+                int range1 = width + 1;
+                while (equal < width)
+                {
+                    Random rnd = new Random();
+                    int x = rnd.Next(1, range1);
+                    equal += x + 1;
+                    range1 -= x + 1;
+
+                    Label label = new Label();
+                    label.TextAlign = ContentAlignment.MiddleRight;
+                    label.Text = $"{x}";
+                    tableleft.Controls.Add(label, col, i);
+                    col--;
+                }
+            }
+
+            //top labels
+            for (int i = 0; i < width; i++)
+            {
+                int all = 0;
+                int row = 4;
+                int range2 = height + 1;
+                while (all < height)
+                {
+                    Random rnd = new Random();
+                    int x = rnd.Next(1, range2);
+                    all += x + 1;
+                    range2 -= x + 1;
+
+                    Label label = new Label();
+                    label.TextAlign = ContentAlignment.BottomCenter;
+                    label.Text = $"{x}";
+                    tabletop.Controls.Add(label, i, row);
+                    row--;
+                }
+            }
+        }
+
+        void generate_board(int width, int height)
+        {
+            TableLayoutPanel newtable = new TableLayoutPanel();
+            newtable.RowCount = height;
+            newtable.ColumnCount = width;
+            buttons = new Button[width * height];
+
+            for (int i = 0; i < width * height; i++)
+            {
+                Random rnd = new Random();
+                int x = rnd.Next(0, 2);
+
+                buttons[i] = new Button()
+                {
+                    Dock = DockStyle.Fill,
+                    FlatStyle = FlatStyle.Flat,
+                    Margin = Padding.Empty,
+                    BackColor = (x == 0) ? Color.Black : Color.White
+                };
+                buttons[i].Click += button_Click;
+                tableLayoutPanel1.Controls.Add(buttons[i], i / width, i % height);
+
+                //button_Click(buttons[i], e);
+            }
+
+            TableLayoutPanel newleft = new TableLayoutPanel();
+            newleft.RowCount = height;
+            newleft.ColumnCount = width / 2;
+            for (int i = 0; i < height; i++)
+            {
+                int equal = 0;
+                int col = 4;
+                int range1 = width + 1;
+                while (equal < width)
+                {
+                    Random rnd = new Random();
+                    int x = rnd.Next(1, range1);
+                    equal += x + 1;
+                    range1 -= x + 1;
+
+                    Label label = new Label();
+                    label.TextAlign = ContentAlignment.MiddleRight;
+                    label.Text = $"{x}";
+                    tableleft.Controls.Add(label, col, i);
+                    col--;
+                }
+            }
+
+            TableLayoutPanel newtop = new TableLayoutPanel();
+            newtop.RowCount = width;
+            newtop.ColumnCount = height / 2;
+            for (int i = 0; i < width; i++)
+            {
+                int all = 0;
+                int row = 4;
+                int range2 = height + 1;
+                while (all < height)
+                {
+                    Random rnd = new Random();
+                    int x = rnd.Next(1, range2);
+                    all += x + 1;
+                    range2 -= x + 1;
+
+                    Label label = new Label();
+                    label.TextAlign = ContentAlignment.BottomCenter;
+                    label.Text = $"{x}";
+                    tabletop.Controls.Add(label, i, row);
+                    row--;
+                }
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.BackColor == Color.White)
+                button.BackColor = Color.Black;
+            else
+                button.BackColor = Color.White;
+        }
+
+        private void NewGameItem1_Click(object sender, EventArgs e)
+        {
+             newForm = new Form();
+            newForm.Text = "New Random Puzzle";
+
+            width = new TextBox();
+            height = new TextBox();
+            width.Location = new Point(150, 50);
+            height.Location = new Point(150, 100); ;
+            newForm.Controls.Add(width);
+            newForm.Controls.Add(height);
+
+            Label wl = new Label();
+            Label hl = new Label();
+            wl.Location = new Point(50, 100);
+            hl.Location = new Point(50, 50);
+            wl.AutoSize = true;
+            wl.Text = "Width";
+            hl.Text = "Height";
+            newForm.Controls.Add(wl);
+            newForm.Controls.Add(hl);
+
+            Button ok = new Button();
+            ok.Location = new Point(50, 200);
+            ok.Height = 40;
+            ok.Text = "OK";
+            ok.Click += ok_Click;
+            newForm.Controls.Add(ok);
+
+            Button cancel = new Button();
+            cancel.Location = new Point(150, 200);
+            cancel.Height = 40;
+            cancel.Text = "Cancel";
+            cancel.Click += cancel_Click;
+            newForm.Controls.Add(cancel);
+
+            newForm.MinimizeBox = false;
+            newForm.MaximizeBox = false;
+            newForm.FormBorderStyle = FormBorderStyle.FixedDialog;
+            newForm.StartPosition = FormStartPosition.CenterScreen;
+            
+            newForm.Show();
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            int w, h;
+            if (int.TryParse(width.Text, out w) && int.TryParse(height.Text, out h))
+            {
+                if (w < 2 || w > 15 || h < 2 || h > 15)
+                {
+
+                }
+                else
+                {
+                    Form1 NewForm = new Form1();
+                    NewForm.Show();
+                    this.Dispose(false);
+                    Form1_Load(sender, e);
+                    newForm.Close();
+                }
+            }
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.newForm.Close();
+        }
+    }
+}
